d0a46c00e2f44b415bd565bab7e43ea6040fea13
Vova282 <46162252+Vova282@users.noreply.github.com>
2020-05-09 00:09:22 +0300

    Creat controller

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -6,5 +6,13 @@ namespace MyProject\Controllers;
 
 class MainController
 {
+public function main()
+{
+  echo 'Главная страница';
+}
 
+public function sayHello(string $name)
+{
+  echo "Привет " .$name;
+}
 }
